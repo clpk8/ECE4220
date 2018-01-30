d301b8f0494e6ad48dc9dbec840bd712c66023ad
ChunBin <clpk8@mail.missouri.edu>
2018-01-30 11:13:20 -0600

    added templete

--- Lab1/Lab1_week2.c
+++ Lab1/Lab1_week2.c
@@ -1,1 +1,29 @@
+#ifndef MODULE
+#define MODULE 
+#endif
+
+#ifndef __KERNEL__
+#define __KERNEL__ 
+#endif
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+
+int init_module(void)
+{
+
+
+	return 0;
+}
+
+
+
+void cleanup_module(void)
+{
+
+
+}
+
 
