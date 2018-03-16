7c71f640a647007eda2f7a1ef5b4ccd40513c246
linChunbin <Ben@mwc-077136.dhcp.missouri.edu>
2018-03-16 11:43:24 -0500

    transfer

--- Lab4/Lab4/main.c
+++ Lab4/Lab4/main.c
@@ -29,49 +29,44 @@ buffer globel;
 sem_t mutex, mutex2;
 void childThread(void* ptr){
     buffer info;
-    
+    info.GPSdataB4 = globel.GPSdataB4;
+    info.GPStimeB4.tv_sec = globel.GPStimeB4.tv_sec;
+    info.GPStimeB4.tv_usec = globel.GPStimeB4.tv_usec;
+    info.buttonPressTime.tv_sec = globel.buttonPressTime.tv_sec;
+    info.buttonPressTime.tv_usec = globel.buttonPressTime.tv_usec;
+
     while(1){
-        
-        info.GPSdataB4 = globel.GPSdataB4;
-        info.GPStimeB4.tv_sec = globel.GPStimeB4.tv_sec;
-        info.GPStimeB4.tv_usec = globel.GPStimeB4.tv_usec;
-        info.buttonPressTime.tv_sec = globel.buttonPressTime.tv_sec;
-        info.buttonPressTime.tv_usec = globel.buttonPressTime.tv_usec;
-        
-        
-        
-        if(info.buttonPressTime.tv_usec != globel.buttonPressTime.tv_usec && info.GPStimeB4.tv_usec != globel.GPStimeB4.tv_usec){
-            
-            info.GPSdataAfter = globel.GPSdataB4;
-            info.GPStimeAfter.tv_sec = globel.GPStimeB4.tv_sec;
-            info.GPStimeAfter.tv_usec = globel.GPStimeB4.tv_usec;
-            
-            
-            //interpolation
-            
-            float x2_x1 = (info.buttonPressTime.tv_sec - info.GPStimeB4.tv_sec)*1000000+(info.buttonPressTime.tv_usec - info.GPStimeB4.tv_usec);
-            float y3_y1 = (float)(info.GPSdataAfter) - (float)(info.GPSdataB4);
-            float x3_x1 = (info.GPStimeAfter.tv_sec - info.GPStimeB4.tv_sec)*1000000+(info.GPStimeAfter.tv_usec - info.GPStimeB4.tv_usec);
-            
-            float y2 = (x2_x1 * y3_y1)/x3_x1 + (float)(info.GPSdataB4);
-            
-            info.GPSdataRealTime = y2;
-            
-            
-            //   sem_wait(&mutex);
-            printf("GPS before:      %u, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataB4,info.GPStimeB4.tv_sec,info.GPStimeB4.tv_usec);
-            printf("GPS during event:%lf, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataRealTime,info.buttonPressTime.tv_sec,info.buttonPressTime.tv_usec);
-            printf("GPS after:       %u, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataAfter,info.GPStimeAfter.tv_sec,info.GPStimeAfter.tv_usec);
-            //     sem_post(&mutex2);
-            
-            
+     //   usleep(250);
+        if(info.GPStimeB4.tv_usec != globel.GPStimeB4.tv_usec){//when different
+            break;
         }
+    }
+    info.GPSdataAfter = globel.GPSdataB4;
+    info.GPStimeAfter.tv_sec = globel.GPStimeB4.tv_sec;
+    info.GPStimeAfter.tv_usec = globel.GPStimeB4.tv_usec;
 
     
+    
+    //interpolation
+    
+    float x2_x1 = (info.buttonPressTime.tv_sec - info.GPStimeB4.tv_sec)*1000000+(info.buttonPressTime.tv_usec - info.GPStimeB4.tv_usec);
+    float y3_y1 = (float)(info.GPSdataAfter) - (float)(info.GPSdataB4);
+    float x3_x1 = (info.GPStimeAfter.tv_sec - info.GPStimeB4.tv_sec)*1000000+(info.GPStimeAfter.tv_usec - info.GPStimeB4.tv_usec);
+    
+    float y2 = (x2_x1 * y3_y1)/x3_x1 + (float)(info.GPSdataB4);
+    
+    info.GPSdataRealTime = y2;
+    
         
-    }
-}
+     //   sem_wait(&mutex);
+        printf("GPS before:      %u, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataB4,info.GPStimeB4.tv_sec,info.GPStimeB4.tv_usec);
+        printf("GPS during event:%lf, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataRealTime,info.buttonPressTime.tv_sec,info.buttonPressTime.tv_usec);
+        printf("GPS after:       %u, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataAfter,info.GPStimeAfter.tv_sec,info.GPStimeAfter.tv_usec);
+   //     sem_post(&mutex2);
 
+
+    
+}
 void writeToBuffer(void* ptr){
     //protect and write to buffer
 
@@ -99,10 +94,13 @@ void writeToBuffer(void* ptr){
         
         globel.buttonPressTime.tv_sec = temp.buttonPressTime.tv_sec;
         globel.buttonPressTime.tv_usec = temp.buttonPressTime.tv_usec;
-        if(i < 4){
+        for(i = 0; i < 4; i++){
             pthread_create(&child[i],NULL,(void*)& childThread, NULL);
         }
-        i++;
+        for(i = 0; i < 4; i++){
+            pthread_join(child[i],NULL);
+        }
+        i=0;
     }
     
     
