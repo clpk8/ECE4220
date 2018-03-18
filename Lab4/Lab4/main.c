1f1d94c1a2c4473f4bb9d33ebe3a81fb51ba8ddf
linChunbin <Ben@mwc-077136.dhcp.missouri.edu>
2018-03-17 19:30:05 -0500

    transfer

--- Lab4/Lab4/main.c
+++ Lab4/Lab4/main.c
@@ -29,43 +29,40 @@ buffer globel;
 sem_t mutex, mutex2, mutex3;
 void childThread(void* ptr){
     
-    buffer info;
+    
+    buffer *info;
+    info = (buffer*) ptr;
+    
     while(1){
-        sem_wait(&mutex2);
+      //  sem_wait(&mutex2);
         sem_wait(&mutex3);
-        info.GPSdataB4 = globel.GPSdataB4;
-        info.GPStimeB4.tv_sec = globel.GPStimeB4.tv_sec;
-        info.GPStimeB4.tv_usec = globel.GPStimeB4.tv_usec;
-        info.buttonPressTime.tv_sec = globel.buttonPressTime.tv_sec;
-        info.buttonPressTime.tv_usec = globel.buttonPressTime.tv_usec;
-        
         while(1){
             //   usleep(250);
-            if(info.GPStimeB4.tv_usec != globel.GPStimeB4.tv_usec){//when different
+            if(info->GPStimeB4.tv_usec != globel.GPStimeB4.tv_usec){//when different
                 break;
             }
         }
-        info.GPSdataAfter = globel.GPSdataB4;
-        info.GPStimeAfter.tv_sec = globel.GPStimeB4.tv_sec;
-        info.GPStimeAfter.tv_usec = globel.GPStimeB4.tv_usec;
+        info->GPSdataAfter = globel.GPSdataB4;
+        info->GPStimeAfter.tv_sec = globel.GPStimeB4.tv_sec;
+        info->GPStimeAfter.tv_usec = globel.GPStimeB4.tv_usec;
         
         
         
         //interpolation
             
-            float x2_x1 = (info.buttonPressTime.tv_sec - info.GPStimeB4.tv_sec)*1000000+(info.buttonPressTime.tv_usec - info.GPStimeB4.tv_usec);
-            float y3_y1 = (float)(info.GPSdataAfter) - (float)(info.GPSdataB4);
-            float x3_x1 = (info.GPStimeAfter.tv_sec - info.GPStimeB4.tv_sec)*1000000+(info.GPStimeAfter.tv_usec - info.GPStimeB4.tv_usec);
+            float x2_x1 = (info->buttonPressTime.tv_sec - info->GPStimeB4.tv_sec)*1000000+(info->buttonPressTime.tv_usec - info->GPStimeB4.tv_usec);
+            float y3_y1 = (float)(info->GPSdataAfter) - (float)(info->GPSdataB4);
+            float x3_x1 = (info->GPStimeAfter.tv_sec - info->GPStimeB4.tv_sec)*1000000+(info->GPStimeAfter.tv_usec - info->GPStimeB4.tv_usec);
             
-            float y2 = (x2_x1 * y3_y1)/x3_x1 + (float)(info.GPSdataB4);
+            float y2 = (x2_x1 * y3_y1)/x3_x1 + (float)(info->GPSdataB4);
             
-            info.GPSdataRealTime = y2;
+            info->GPSdataRealTime = y2;
             
             
             //   sem_wait(&mutex);
-            printf("GPS before:      %u, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataB4,info.GPStimeB4.tv_sec,info.GPStimeB4.tv_usec);
-            printf("GPS during event:%lf, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataRealTime,info.buttonPressTime.tv_sec,info.buttonPressTime.tv_usec);
-            printf("GPS after:       %u, time in second:%ld, time in microsecond:%d\n\n",info.GPSdataAfter,info.GPStimeAfter.tv_sec,info.GPStimeAfter.tv_usec);
+            printf("GPS before:      %u, time in second:%ld, time in microsecond:%d\n\n",info->GPSdataB4,info->GPStimeB4.tv_sec,info->GPStimeB4.tv_usec);
+            printf("GPS during event:%lf, time in second:%ld, time in microsecond:%d\n\n",info->GPSdataRealTime,info->buttonPressTime.tv_sec,info->buttonPressTime.tv_usec);
+            printf("GPS after:       %u, time in second:%ld, time in microsecond:%d\n\n",info->GPSdataAfter,info->GPStimeAfter.tv_sec,info->GPStimeAfter.tv_usec);
         
             sem_post(&mutex3);
 
@@ -97,17 +94,19 @@ void writeToBuffer(void* ptr){
 
     
     while(1){
+        
         for(i = 0; i < 4; i++){
-            sem_wait(&mutex);
+         //   sem_wait(&mutex);
             
             if(read(pipe_N_pipe2,&temp,sizeof(temp)) != sizeof(temp)){
                 printf("N_pipe2 reading1 error\n");
+                exit(-1);
             }
-            
-            pthread_create(&child[i],NULL,(void*)& childThread, NULL);
-            globel.buttonPressTime.tv_sec = temp.buttonPressTime.tv_sec;
-            globel.buttonPressTime.tv_usec = temp.buttonPressTime.tv_usec;
-            sem_post(&mutex2);
+            temp.GPSdataB4 = globel.GPSdataB4;
+            temp.GPStimeB4.tv_sec = globel.GPStimeB4.tv_sec;
+            temp.GPStimeB4.tv_usec = globel.GPStimeB4.tv_usec;
+
+            pthread_create(&child[i],NULL,(void*)& childThread, (void*)&temp);
         }
         
     }
@@ -149,7 +148,7 @@ int main(int argc, const char * argv[]) {
         globel.GPSdataB4 = temp;
         globel.GPStimeB4.tv_sec = GPStime.tv_sec;
         globel.GPStimeB4.tv_usec = GPStime.tv_usec;
-        sem_post(&mutex);
+       // sem_post(&mutex);
 
         
     }
