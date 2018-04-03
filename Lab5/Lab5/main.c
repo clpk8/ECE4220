8f19931641ba4f6caff8035f0c30052d0578ee9e
ChunBin <clpk8@mail.missouri.edu>
2018-04-03 10:43:05 -0500

    transfer

--- Lab5/Lab5/main.c
+++ Lab5/Lab5/main.c
@@ -22,7 +22,7 @@
 
 
 int main(int argc, const char * argv[]) {
-    
+
     int sock, length, n;
     int boolval = 1; //use for socket option, to allow broadcast
     //receive should be empty, it will been fill up
@@ -30,7 +30,7 @@ int main(int argc, const char * argv[]) {
     char buf[MSG_SIZE];
     socklen_t fromlen;
     struct ifreq ifr;
-    
+
     unsigned char ip_address[13];
 
     /*AF_INET - to define IPv4 Address type.*/
@@ -38,45 +38,45 @@ int main(int argc, const char * argv[]) {
     /*wlan0 - define the ifr_name - port name
      where network attached.*/
     memcpy(ifr.ifr_name, "wlan0", IFNAMSIZ-1);
-    
-    
+
+
     if(argc < 2){
-        
+
         printf("Error, Please enter the port number");
         exit(-1);
     }
-    
+
     sock = socket(AF_INET, SOCK_DGRAM, 0); // Creates socket. Connectionless.
     if (sock < 0)
         error("Opening socket");
-    
+
     length = sizeof(receive);            // length of structure
     bzero(&receive,length);            // sets all values to zero. memset() could be used
-    
+
     length = sizeof(server);            // length of structure
     bzero(&server,length);            // sets all values to zero. memset() could be used
-    
+
     //initilize the server
     server.sin_family = AF_INET;        // symbol constant for Internet domain
     server.sin_addr.s_addr = INADDR_ANY;        // IP address of the machine on which
     // the server is running
     server.sin_port = htons(atoi(argv[1]));    // port number
-    
+
     /*Accessing network interface information by
      passing address using ioctl.*/
     ioctl(sock, SIOCGIFADDR, &ifr);
-    
+
     strcpy(ip_address,inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-    
+
     printf("System IP Address is: %s\n",ip_address);
-    
-    
-    
-    
+
+
+
+
     // binds the socket to the address of the host and the port number
     if (bind(sock, (struct sockaddr *)&server, length) < 0)
         error("binding");
-    
+
     // change socket permissions to allow broadcast
     if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &boolval, sizeof(boolval)) < 0)
     {
@@ -85,23 +85,22 @@ int main(int argc, const char * argv[]) {
     }
 
     fromlen = sizeof(struct sockaddr_in);    // size of structure
-
     while(1){
-        
+
         bzero(buf,MSG_SIZE);        // sets all values to zero. memset() could be used
         n = recvfrom(sock, buf, MSG_SIZE, 0, (struct sockaddr *)&clint, &fromlen);
         if (n < 0)
             error("recvfrom");
-        
-        printf("Message received is %s", buf);
-        
-        
-        
-    }
-    
-    
-    
 
-    
+        printf("Message received is %s", buf);
+
+
+
+    }
+
+
+
+
+
     return 0;
 }
