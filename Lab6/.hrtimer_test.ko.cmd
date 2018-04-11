9f7489b91f3488d3b727dd5eaae6e101f81b8bec
ChunBin <clpk8@mail.missouri.edu>
2018-04-11 11:27:33 -0500

    transfer

---
+++ Lab6/.hrtimer_test.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/ECE4220/Lab6/hrtimer_test.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/ECE4220/Lab6/hrtimer_test.ko /home/pi/ECE4220/Lab6/hrtimer_test.o /home/pi/ECE4220/Lab6/hrtimer_test.mod.o
---
+++ Lab6/.kthread_test.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/ECE4220/Lab6/kthread_test.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/ECE4220/Lab6/kthread_test.ko /home/pi/ECE4220/Lab6/kthread_test.o /home/pi/ECE4220/Lab6/kthread_test.mod.o
