44f87abbcbf42a45e09d69d22a46c87d5d710a67
linChunbin <Ben@mwc-077045.dhcp.missouri.edu>
2018-04-12 15:21:10 -0500

    transfer

--- Lab6/kthread_test.c
+++ Lab6/kthread_test.c
@@ -62,11 +62,11 @@ int kthread_fn(void *ptr)
 //        set = set + (0x001c / 4);    //GPIO Pin Output Set 0
 //        *set = *set & 0x40;
         //test
-        set = set + (0x001c/4); //gpset the pin to 1
+        set = set + (0x001c/4); //use GPSET1 register
         *set = *set & 0x3C; //set 4 leds to 1        00x 0011 1100
         
         
-		msleep(1);	// good for > 10 ms
+		msleep(100);	// good for > 10 ms
 		//msleep_interruptible(1000); // good for > 10 ms
 		//udelay(unsigned long usecs);	// good for a few us (micro s)
 		//usleep_range(unsigned long min, unsigned long max); // good for 10us - 20 ms
@@ -78,7 +78,7 @@ int kthread_fn(void *ptr)
         set = set + (0x0028 / 4);    //GPIO Pin Output clear 0
         *set = *set | 0x003c;    //set the Led pins to 1 -> ...00000111100
         
-        msleep(1);
+        msleep(100);
 		// In an infinite loop, you should check if the kthread_stop
 		// function has been called (e.g. in clean up module). If so,
 		// the kthread should exit. If this is not done, the thread
